4bc3087cf78d4eb255efe4f799a5d5639b6643f0
Shamkhan Mutuskhanov <spantset@icloud.com>
2023-05-14 13:07:51 +0300

    Made circles from View squares

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var trafficLightRedView: UIView!
+    @IBOutlet var trafficLightYellowView: UIView!
+    @IBOutlet var trafficLightGreenView: UIView!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        trafficLightRedView.layer.cornerRadius = trafficLightRedView.bounds.height / 2
+        trafficLightYellowView.layer.cornerRadius = trafficLightYellowView.bounds.height / 2
+        trafficLightGreenView.layer.cornerRadius = trafficLightGreenView.bounds.height / 2
     }
 
-
+    @IBAction func nextButtonDidTapped() {
+        
+    }
+    
 }
 
